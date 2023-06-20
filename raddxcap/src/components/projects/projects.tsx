c4498e34ba7a36c2e8a3afb263400384b02ef533
Madeline <madelinejgowan@gmail.com>
2023-06-20 10:11:08 -0700

    Project section work

--- raddxcap/src/components/projects/projects.tsx
+++ raddxcap/src/components/projects/projects.tsx
@@ -1,30 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import CaretPrev from '@mui/icons-material/ArrowBackIos';
 import CaretNext from '@mui/icons-material/ArrowForwardIos';
 
-export default function projects() {
+export default function Projects() {
 
-  // const [currentIndex, setCurrentIndex] = useState(0);
-  // const length = logo.boardAdvisory.length;
-  // const carouselInfiniteScroll = () => {
-  //     if (currentIndex >= length - 3) {
-  //         return setCurrentIndex(0);
-  //     }
-  //     return setCurrentIndex(currentIndex + 1)
-  // }
-  // useEffect(() => {
-  //     const interval = setInterval(() => { carouselInfiniteScroll() }, 5000);
-  //     return () => clearInterval(interval);
-  // })
-  // const scrollPrev = () => {
-  //     const newIndex = currentIndex - 1;
-  //     setCurrentIndex(newIndex < 0 ? length - 3 : newIndex)
-  // }
-  // const scrollNext = () => {
-  //     const newIndex = currentIndex + 1;
-  //     setCurrentIndex(newIndex >= length - 2 ? 0 : newIndex)
-  // }
+  const properties = [
+      {
+        id: 1,
+        desc: 'hiiiiiiiiii',
+        imageUrl: '../../assets/House 1.png',
+        webUrl: ''
+      },
+      {
+        id: 2,
+        desc: 'HEY HEY HEY HEY',
+        imageUrl: '../../assets/House 2.png',
+        webUrl: ''
+      },
+      {
+        id: 3,
+        desc: 'whoa',
+        imageUrl: '/assets/House 1.png',
+        webUrl: ''
+      },
+      {
+        id: 4,
+        desc: 'WHOA THERE',
+        imageUrl: '/assets/House 2.png',
+        webUrl: ''
+      },
+      {
+        id: 5,
+        desc: '',
+        imageUrl: '',
+        webUrl: ''
+      },
+      {
+        id: 6,
+        desc: '',
+        imageUrl: '',
+        webUrl: ''
+      }
+    ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const length = properties.length;
+  const carouselInfiniteScroll = () => {
+      if (currentIndex >= length - 3) {
+          return setCurrentIndex(0);
+      }
+      return setCurrentIndex(currentIndex + 1)
+  }
+  useEffect(() => {
+      const interval = setInterval(() => { carouselInfiniteScroll() }, 5000);
+      return () => clearInterval(interval);
+  })
+  const scrollPrev = () => {
+      const newIndex = currentIndex - 1;
+      setCurrentIndex(newIndex < 0 ? length - 3 : newIndex)
+  }
+  const scrollNext = () => {
+      const newIndex = currentIndex + 1;
+      setCurrentIndex(newIndex >= length - 2 ? 0 : newIndex)
+  }
 
   return (
     <div>
@@ -40,21 +79,20 @@ export default function projects() {
 
           </div>
           {
-            logo.boardAdvisory.map((member) => (
-              <div key={member.id} className='carItem' style={{ transform: `translate(-${currentIndex * 100}%)` }}>
+            properties.map((property) => (
+              <div key={property.id} className='carItem' style={{ transform: `translate(-${currentIndex * 100}%)` }}>
                 <div className='carCard card'>
-                  <a href={member.webUrl} className='carImgContainer'>
-                    <img src={member.logoUrl} className='d-block w-100 carImg' alt={member.name} />
+                  <a href={property.webUrl} className='carImgContainer'>
+                    <img src={property.imageUrl} className='d-block w-100 carImg' />
                   </a>
-
                   <div className='carBody'>
-                    <p className='carText carTitle'>{member.name}</p>
-                    <p className='card-text carText'>{member.desc}</p>
+                    <p className='card-text carText'>{property.desc}</p>
                   </div>
                 </div>
               </div>
             ))
           }
+
         </div>
       </div>
     </div>
