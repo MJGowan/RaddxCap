4f5dba1b51caafb0560f2d7fb4431333d8f461b9
Madeline <madelinejgowan@gmail.com>
2023-06-23 11:44:37 -0700

    Basic footer outline

--- raddxcap/src/App.tsx
+++ raddxcap/src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       <Mid/>
       <Projects/>
       <Contacts/>
+      <Footer/>
     </div>
   );
 }
--- raddxcap/src/components/footer/footer.tsx
+++ raddxcap/src/components/footer/footer.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import {Container, Row, Col} from 'react-bootstrap';
 
 export default function footer() {
+  const fbLogo = require('../../assets/facebook-light.png');
+  const twLogo = require('../../assets/twitter-light.png');
+  const liLogo = require('../../assets/linkedin-light.png');
+  const inLogo = require('../../assets/insta-light.png');
+
   return (
-    <div>footer</div>
+      <div className='mt-5'>
+        <Row className='footerContainer'>
+          <Container>
+            <Row>
+            <Col>
+              <p>Las Vegas, NV 89131</p>
+              <p>(209) 313-9953</p>
+              <p>RaddCapitalBecky@gmail.com</p>
+          </Col>
+          <Col >
+            <p className='d-flex justify-content-end'>Admin</p>
+            <div className='d-flex justify-content-end'>
+              <img src={fbLogo} className='logos' />
+              <img src={twLogo} className='logos' />
+              <img src={liLogo} className='logos' />
+              <img src={inLogo} className='logos' />
+            </div>
+            <p></p>
+            <p className='d-flex justify-content-end'>Site designed and developed by Madeline Gowan</p>
+          </Col>
+            </Row>
+          </Container>
+        </Row>
+      </div>
   )
 }
