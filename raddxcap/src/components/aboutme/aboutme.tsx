1a0464454b3fe333408978d29e6b10b1c92aac5f
Madeline <madelinejgowan@gmail.com>
2023-07-27 09:48:28 -0700

    New Transparent Bg Becky

--- raddxcap/src/components/aboutme/aboutme.tsx
+++ raddxcap/src/components/aboutme/aboutme.tsx
@@ -4,6 +4,7 @@ import './aboutme.css';
 
 export default function aboutme() {
   const becky = require('../../assets/C4724FB2-.jpg');
+  const mom = require('../../assets/mom.png');
   return (
     <div>
       <Container id='about'>
@@ -15,9 +16,8 @@ export default function aboutme() {
             <p className='lato about-txt'>With a deep-rooted commitment to making a positive impact, and an extensive background in diverse industries, RaddxCap is your trusted partner for dynamic investments. When it comes to real estate opportunities, we have a deep understanding of the market dynamics and a keen eye for spotting potential. We assist both investors and business owners in identifying and acquiring properties with a comprehensive approach that encompasses due diligence, financial analysis, negotiation, and funding, ensuring a seamless transition and setting the stage for long-term success.</p>
           </div>
 
-
-          <div className='holdBecky'>
-            <img src={becky} className='becky' />
+          <div className='holdMom'>
+            <img src={mom} className='mom' />
           </div>
 
         </Row>
